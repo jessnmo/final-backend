7822aad6ce2d91c5e2b12737aa7e1619d59bc0c1
jessnmo <jessicamo92@gmail.com>
2022-12-28 17:26:35 +0100

    accessToken

---
+++ endpoints/authenticateUser.js
@@ -0,0 +1,21 @@
+const User = require('../Schemas/user');
+
+export const authenticateUser = async (req, res, next) => {
+	const accessToken = req.header('Authorization');
+	try {
+		const user = await User.findOne({ accessToken });
+		if (user) {
+			next();
+		} else {
+			res.status(401).json({
+				success: false,
+				response: 'Please log in',
+			});
+		}
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			response: error.err,
+		});
+	}
+};
